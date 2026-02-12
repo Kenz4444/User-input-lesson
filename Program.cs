582a8d7cb2ac30e29fe40ef1c86116c8c30e96b9
Mackenzie McIntyre <362574782@GoTVDSB.ca>
2026-02-12 12:11:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace User_input_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
