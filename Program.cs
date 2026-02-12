1102c42cab92ea879fb43ba335fa579c0276505b
Mackenzie McIntyre <362574782@GoTVDSB.ca>
2026-02-12 13:13:56 -0500

    I need help real bad

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace User_input_lesson
+﻿using System.Transactions;
+
+namespace User_input_lesson
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //practice one
+            string username;
+
+            Console.Write("Enter username: ");
+            Console.Write("");
+            username = Console.ReadLine();
+            Console.WriteLine("Your username is " + username);
+            Console.ReadLine();
+
+            //practice two
+            int age;
+            Console.Write("Enter your age: ");
+            Console.Write("");
+            Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
+            Console.WriteLine("Your age is: " + age);
+            Console.ReadLine() ;
+
+            //practice three
+            double price;
+            
+            Console.Write("What is the price?");
+            Console.Write("");
+            double.TryParse(Console.ReadLine(),out price);
+           
+            Console.WriteLine("The price is " + price.ToString("C"));
+
+
+
         }
     }
 }
